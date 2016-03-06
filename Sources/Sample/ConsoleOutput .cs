e6c9afd8ad006cbf151d9727f5a51085912f7ea8
SergerGood <serzher@inbox.ru>
2016-03-06 20:57:37 +0300

    Application Startup

---
+++ Sources/Sample/ConsoleOutput .cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample
+{
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
---
+++ Sources/Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+
+namespace Sample
+{
+    class Program
+    {
+        public static IContainer Container { get; set; }
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+
+            Container = builder.Build();
+        }
+    }
+}
